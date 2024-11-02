5dd54fb042f4cd5bb5d34bdeb507aef6a6d6b1f9
Ashzuu <evandynak@icloud.com>
2024-11-02 12:51:57 +0100

    fix: change encoding to utf-8

--- Python/logs.py
+++ Python/logs.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
 def MakeLogs():
-    with open(r"logs.txt","a") as file:
+    with open(r"logs.txt","a", encoding="utf-8") as file:
         file.write(f"Lancement de l'analyse à {datetime.now().strftime(("%d/%m/%Y à %H:%M:%S"))}\n")
         file.close()
 def WriteInLogs(message:str):
-    with open(r"logs.txt","a") as file:
+    with open(r"logs.txt","a", encoding="utf-8") as file:
         file.write(message + "\n")
         file.close()
